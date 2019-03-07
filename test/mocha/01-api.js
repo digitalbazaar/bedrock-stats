9d2a2873a529997cdc94fbf42ceca0896054a217
Matthew Collier <collier.matthew@gmail.com>
2019-03-07 09:49:22 -0500

    Add getMonitorIds API.

--- test/mocha/01-api.js
+++ test/mocha/01-api.js
@@ -50,7 +50,51 @@ describe('Stats', () => {
       should.exist(result);
       result.should.be.an('array');
     });
-  });
+  }); // end getReports API
+
+  describe('getMonitorIDs API', () => {
+    it('throws AssertionError on missing `storageApi` param', async () => {
+      let error;
+      let result;
+      try {
+        result = await brStats.getMonitorIds({});
+      } catch(e) {
+        error = e;
+      }
+      should.not.exist(result);
+      should.exist(error);
+      (error instanceof AssertionError).should.be.true;
+    });
+    it('throws NotFoundError on an unknown storage API', async () => {
+      let error;
+      let result;
+      try {
+        result = await brStats.getMonitorIds({storageApi: 'unknown'});
+      } catch(e) {
+        error = e;
+      }
+      should.not.exist(result);
+      should.exist(error);
+      error.name.should.equal('NotFoundError');
+    });
+    it('returns a result', async () => {
+      // allow the mock monitor to generate some reports
+      await sleep(500);
+      let error;
+      let result;
+      try {
+        result = await brStats.getMonitorIds({storageApi: 'redis'});
+      } catch(e) {
+        error = e;
+      }
+      assertNoError(error);
+      should.exist(result);
+      result.should.be.an('array');
+      result.should.have.length(1);
+      result.should.have.same.members(['fooMonitor']);
+    });
+  }); // end getMonitorIDs API
+
   describe('Scheduled Job', () => {
     it('stores monitor reports using redis storage', async () => {
       await sleep(5000);
